e31b46de9dc804ddfb1786aa0d08381142eed0d6
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2025-01-13 14:15:07 +0100

    Adding spanish translation

---
+++ bober/assets/i18n/es_ES.ts
@@ -0,0 +1,217 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>main</name>
+    <message>
+        <source>Quitting application</source>
+        <translation>Solicitud de baja</translation>
+    </message>
+</context>
+
+<context>
+    <name>detail</name>
+    <message>
+        <source>File Details</source>
+        <translation>Detalles del archivo</translation>
+    </message>
+    <message>
+        <source>File: </source>
+        <translation>Archivo: </translation>
+    </message>
+    <message>
+        <source>Replicas: </source>
+        <translation>Réplicas: </translation>
+    </message>
+    <message>
+        <source>Collection: </source>
+        <translation>Colección: </translation>
+    </message>
+    <message>
+        <source>Creation time: </source>
+        <translation>Tiempo de creación: </translation>
+    </message>
+    <message>
+        <source>Modified time: </source>
+        <translation>Tiempo modificado: </translation>
+    </message>
+    <message>
+        <source>Size: </source>
+        <translation>Talla: </translation>
+    </message>
+</context>
+<context>
+    <name>window</name>
+    <message>
+        <source>Back</source>
+        <translation>Volver</translation>
+    </message>
+    <message>
+        <source>Filter...</source>
+        <translation>Filtro...</translation>
+    </message>
+    <message>
+        <source>Explorer</source>
+        <translation>Explorador</translation>
+    </message>
+    <message>
+        <source>Application Started</source>
+        <translation>Aplicación iniciada</translation>
+    </message>
+    <message>
+        <source>logged in</source>
+        <translation>conectado</translation>
+    </message>
+    <message>
+        <source>Open File</source>
+        <translation>Abrir archivo</translation>
+    </message>
+    <message>
+        <source>Permission denied.</source>
+        <translation>Permiso denegado.</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>Archivo</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Conectarse</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Deja</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Configuración</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Ayuda</translation>
+    </message>
+    <message>
+        <source>About Qt</source>
+        <translation>Sobre Qt</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>Descargar</translation>
+    </message>
+    <message>
+        <source>Save to folder</source>
+        <translation>Guardar en carpeta</translation>
+    </message>
+    <message>
+        <source>downloaded</source>
+        <translation>descargado</translation>
+    </message>
+
+    <!--Help texts-->
+    <message>
+        <source>Welcome to </source>
+        <translation>Bienvenido a </translation>
+    </message>
+    <message>
+        <source>Before doing anything, please be sure to update the settings of the application. To do this, go to 'Edit'->'Settings'.</source>
+        <translation>Antes de hacer nada, asegúrate de actualizar la configuración de la aplicación. Para ello, vaya a 'Editar'->'Configuración'.</translation>
+    </message>
+    <message>
+        <source>Then, you can login with the login window dialog. Go to 'File'->'Login'.</source>
+        <translation>A continuación, puede iniciar sesión con el diálogo de la ventana de inicio de sesión. Vaya a 'Archivo'->'Iniciar sesión'.</translation>
+    </message>
+    <message>
+        <source>Once logged in you can enjoy browsing your irods instance like a regular system file explorer. You can also download those file be right-clicking and hitting the 'Download' button.</source>
+        <translation>Una vez iniciada la sesión, podrás navegar por tu instancia de irods como si se tratara de un explorador de archivos normal. También puede descargar esos archivos haciendo clic con el botón derecho del ratón y pulsando el botón 'Descargar'.</translation>
+    </message>
+
+</context>
+<context>
+    <name>settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Configuración</translation>
+    </message>
+    <message>
+        <source>Config path:</source>
+        <translation>Ruta de configuración:</translation>
+    </message>
+    <message>
+        <source>Host:</source>
+        <translation>Anfitrión:</translation>
+    </message>
+    <message>
+        <source>Port:</source>
+        <translation>Puerto:</translation>
+    </message>
+    <message>
+        <source>Zone:</source>
+        <translation>Zona:</translation>
+    </message>
+    <message>
+        <source>Root path:</source>
+        <translation>Ruta raíz:</translation>
+    </message>
+    <message>
+        <source>Download notifications:</source>
+        <translation>Descargar notificaciones:</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Salvar</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Select irods configuration</source>
+        <translation>Seleccione la configuración de irods</translation>
+    </message>
+</context>
+<context>
+    <name>progress</name>
+    <message>
+        <source>Download progress</source>
+        <translation>Descargar progreso</translation>
+    </message>
+</context>
+<context>
+    <name>login</name>
+    <message>
+        <source>Login</source>
+        <translation>Conectarse</translation>
+    </message>
+    <message>
+        <source>Confirm</source>
+        <translation>Confirme</translation>
+    </message>
+    <message>
+        <source>Username: </source>
+        <translation>Usuario: </translation>
+    </message>
+    <message>
+        <source>Password: </source>
+        <translation>Contraseña: </translation>
+    </message>
+</context>
+<context>
+    <name>worker</name>
+    <message>
+        <source>does not exist</source>
+        <translation>no existe</translation>
+    </message>
+</context>
+</TS>
